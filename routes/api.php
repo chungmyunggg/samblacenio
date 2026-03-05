b38ea91e1eb63143175f62c11b71d02ffd9199bd
chungmyunggg <samblacenio.johnroland@gmail.com>
2026-03-05 23:01:20 +0800

    integrating Authorization with Spatie Laravel Permissions

--- routes/api.php
+++ routes/api.php
@@ -47,3 +47,17 @@ Route::post('/login', function (Request $request) {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// 8. Protect Routes Using Roles
+Route::middleware(['auth:sanctum','role:admin'])->get('/admin-dashboard', function () {
+    return response()->json([
+        'message' => 'Welcome Admin'
+    ]);
+});
+
+// 9. Protect Routes Using Permission
+Route::middleware(['auth:sanctum','permission:create posts'])->post('/posts', function () {
+    return response()->json([
+        'message' => 'Post created'
+    ]);
+});
